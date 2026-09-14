85297ea23d9697372558278093f9dee971ad9f92
bhoomiverma1244 <bhoomiverma7581@gmail.com>
2026-09-14 14:45:24 +0530

    lab2 prg4

---
+++ lab1/lab2/prg4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void byValue(int x) {
+x = x + 10;
+}
+void byReference(int &x) {
+x = x + 10;
+}
+void byAddress(int *x) {
+*x = *x + 10;
+}
+int main() {
+int a = 10;
+int b = 10;
+int c = 10;
+byValue(a);
+byReference(b);
+byAddress(&c);
+cout << "Call by Value: " << a << endl;
+cout << "Call by Reference: " << b << endl;
+cout << "Call by Address: " << c << endl;
+return 0;
+}
