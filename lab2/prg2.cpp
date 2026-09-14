4833da3d60afd5a54b443181e697519df73af92b
bhoomiverma1244 <bhoomiverma7581@gmail.com>
2026-09-14 14:48:20 +0530

    lab2 prg2

---
+++ lab2/prg2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float salary = 50000;
+    float *newSalary = &salary;   
+    *newSalary = *newSalary + (*newSalary * 10 / 100);
+    cout << "Salary from old variable: " << salary << endl;
+cout << "Salary using pointer: " << *newSalary << endl;
+    return 0;
+}
